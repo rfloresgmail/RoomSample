a0f8d06aeba5af92c01d3bb9fc22c89c5c96eda4
Rocio Flores <rocioflores@Rocios-MacBook-Air.local>
2020-02-05 23:51:05 -0500

    adds model package

---
+++ app/src/main/java/com/example/roomsample/model/ToDoItem.java
@@ -0,0 +1,16 @@
+package com.example.roomsample.model;
+
+public class ToDoItem {
+
+    private String mName;
+
+    private Integer mId;
+
+    public String getName() {
+        return mName;
+    }
+
+    public Integer getId() {
+        return mId;
+    }
+}
